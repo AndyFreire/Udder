72c38984073085039f1794803634fb6d6e07a26d
Andrew Freire <andrew.m.freire@gmail.com>
2020-06-09 20:28:51 -0500

    delete viewDidLoad method

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -15,9 +15,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func viewDidDisappear(_ animated: Bool) {
-        //Code
-    }
 
 }
 
